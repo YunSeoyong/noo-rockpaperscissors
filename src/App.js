dbc0db9cd78606e48326b4763ec6d2a95fe99617
YunSeoyong <seoy2515@gmail.com>
2024-03-18 21:25:18 +0900

    01-04

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import reset from "styled-reset"
+
 import './App.css';
+import Box from './components/Box';
+
+// 1. 박스 2개 (타이틀, 사진, 결과)
+// 2. 가위 바위 보 버튼
+// 3. 버튼 클릭 => 클릭 값이 박스에 보임
+// 4. 컴퓨터는 랜덤하게 아이템을 선택.
+// 5. 3, 4의 결과를 가지고 누가 이겼는지 승패를 가림.
+// 6. 승패 결과에 따라 테두리 색 바뀜. (이김-초록, 짐-빨강, 비김-검정)
+
+const choice = {
+    scissors: {
+        name: "Scissors",
+        img: "https://assets.katogroup.eu/i/katogroup/VT8-0919-24_02_victorinox"
+    },
+    rock: {
+        name: "Rock",
+        img: "https://m.media-amazon.com/images/I/51rOMX5z40L.jpg"
+    },
+    paper: {
+        name: "Paper",
+        img: "https://www.collinsdictionary.com/images/full/paper_111691001.jpg"
+    },
+}
 
 function App() {
+    const [userSelect, setUserSelect] = useState(null);
+
+    const play = (userChoice) => {
+        setUserSelect(choice[userChoice]);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <div className="main">
+            <Box title="You" item={userSelect} />
+            <Box title="Computer" />
+        </div>
+        <div className='main'>
+            <button onClick={() => play('scissors')}>가위</button>
+            <button onClick={() => play('rock')}>바위</button>
+            <button onClick={() => play('paper')}>보</button>
+        </div>
     </div>
   );
 }
 
 export default App;
+
+const GlobalStyled = createGlobalStyle`
+    ${reset}
+`;
---
+++ src/components/Box.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Box = (props) => {
+  return (
+    <div className='box'>
+        <h1>{props.title}</h1>
+        <p className='item-img'>
+            <img src={props.item && props.item.img} alt={props.item && props.item.name} />
+        </p>
+        <h2>WIN</h2>
+    </div>
+  )
+}
+
+export default Box
